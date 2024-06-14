f2a649875801e0de90e4cc587a4b7bbfb5bde9c4
aket0 <moynehubert@gmail.com>
2024-06-14 08:36:37 +0200

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
+
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,73 @@
+"use client"
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.module.css";
+import selfie from "../../public/selfie.png";
+import React, { useState, useEffect } from "react";
+import { Permanent_Marker} from 'next/font/google'
+import { Prata } from "next/font/google";
+import { Proza_Libre } from "next/font/google";
 
-export default function Home() {
+
+
+
+const PermanentMarker = Permanent_Marker({ subsets: ['latin'], weight: ['400'] })
+const Prataa = Prata({ subsets: ['latin'], weight: ['400'] })
+const ProzaLibre = Proza_Libre({ subsets: ['latin'], weight: ['400'] })
+
+const Home: React.FC = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    
+    
+    <div id="main">
+      <header className={scrolled ? "header scrolled" : "header"} id="header">
+        <h1 className={PermanentMarker.className}>Hubert.</h1>
+        <ul>
+          <li><a className={ProzaLibre.className} href="#home">HOME</a></li>
+          <li><a className={ProzaLibre.className} href="#portfolio">PORTFOLIO</a></li>
+          <li><a className={ProzaLibre.className} href="#contact">CONTACT</a></li>
+          <li><a className={ProzaLibre.className} href="https://hubertmoyne.pythonanywhere.com">MEDIA</a></li>
+        </ul>
+      </header>
+      <div id="imgTop">
+        <h1 className={Prataa.className}>Naturaly creative, beautifully coded</h1>
+      </div>
+      <div id="bio">
+        <Image src={selfie} alt="selfie" />
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          My name is Hubert Moyne and I’m a web developer. Truly in love with the web culture and its history. I’m fascinated by how design is important.
+          To create the web of tomorrow, and make it more sustainable, I try to make each of my apps an immersive experience for the pleasure of the user.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <div id="contact">
+        <div id="contactCard">
+          <form method="post">
+            <input type="text" placeholder="Enter your name" />
+            <input type="email" placeholder="Enter your email" />
+            <textarea placeholder="Tell me more about your project"></textarea>
+            <button type="submit">Contact me</button>
+          </form>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+   
   );
-}
+};
+
+export default Home;
